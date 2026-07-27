e85550d9e9e0b1aa0bce731d0979fba1f9f2638e
sapryx <sapryxv@gmail.com>
2026-07-28 01:46:28 +0300

    add more audio player tests

--- Core.Tests/AudioPlayerTests.cs
+++ Core.Tests/AudioPlayerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Playback;
+using Core.Playlists;
 using Moq;
 
 namespace Core.Tests;
@@ -28,12 +29,61 @@ public class AudioPlayerTests
     }
 
     [Fact]
-    public void Volume_Get_Set_DelegatedToAudioEngine()
+    public void Volume_Get_Set_DelegatesToAudioEngine()
     {
         _ = audioPlayer.Volume;
         audioPlayer.Volume = 13;
         
         audioEngineMock.VerifyGet(it => it.Volume, Times.Once);
         audioEngineMock.VerifySet(it => it.Volume = 13, Times.Once);
+    }
+
+    [Fact]
+    public void PlaybackPosition_DelegatesToAudioEngine()
+    {
+        _ = audioPlayer.PlaybackPosition;
+        audioPlayer.PlaybackPosition = 0.4f;
+        
+        audioEngineMock.VerifyGet(it => it.PlaybackPosition, Times.Once);
+        audioEngineMock.VerifySet(it => it.PlaybackPosition = 0.4f, Times.Once);
+    }
+
+    [Fact]
+    public void PlayTrack_SetsNowPlaying()
+    {
+        var playlistItem = new Mock<IPlaylistItem>();
+        
+        audioPlayer.PlayTrack(playlistItem.Object);
+        
+        Assert.Equal(playlistItem.Object, audioPlayer.NowPlaying);
+    }
+
+    [Fact]
+    public void PlayTrack_DelegatesToAudioEngine()
+    {
+        var playlistItem = new Mock<IPlaylistItem>();
+        
+        audioPlayer.PlayTrack(playlistItem.Object);
+        
+        audioEngineMock.Verify(it => it.StartPlayback(playlistItem.Object.AudioTrack), Times.Once);
+    }
+
+    [Fact]
+    public void TogglePause_DelegatesToAudioEngine()
+    {
+        var playlistItem = new Mock<IPlaylistItem>();
+        
+        audioPlayer.PlayTrack(playlistItem.Object);
+        audioPlayer.TogglePause();
+        
+        audioEngineMock.Verify(it => it.TogglePause(), Times.Once);
+    }
+
+    [Fact]
+    public void TogglePause_NothingIsPlaying_NothingHappens()
+    {
+        audioPlayer.TogglePause();
+        
+        audioEngineMock.Verify(it => it.TogglePause(), Times.Never);
     }
 }
