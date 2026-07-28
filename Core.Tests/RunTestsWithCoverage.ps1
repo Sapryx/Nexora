954dcce27e74192d9bb776a187b22cf4e2de8b12
sapryx <sapryxv@gmail.com>
2026-07-28 23:09:22 +0300

    update test coverage script

--- Core.Tests/RunTestsWithCoverage.ps1
+++ Core.Tests/RunTestsWithCoverage.ps1
@@ -1,2 +1,4 @@
-dotnet test --collect:"XPlat Code Coverage"
-reportgenerator -reports:"**/coverage.cobertura.xml" -targetdir:"Core.Tests/Report" -reporttypes:Html
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue Core.Tests/TestResults
+
+dotnet test --collect:"XPlat Code Coverage" --results-directory:"Core.Tests/TestResults"
+reportgenerator -reports:"Core.Tests/TestResults/**/coverage.cobertura.xml" -targetdir:"Core.Tests/Report" -reporttypes:Html
