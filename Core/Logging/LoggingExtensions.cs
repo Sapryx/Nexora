14c0de8388858bc16f0081d87fa102d17c1af0f0
sapryx <sapryxv@gmail.com>
2026-07-28 13:34:57 +0300

    update logging extensions

--- Core/Logging/LoggingExtensions.cs
+++ Core/Logging/LoggingExtensions.cs
@@ -13,12 +13,30 @@ public static class LoggingExtensions
         logger.ZLogTrace(ref message);
     }
 
+    public static void Trace(
+        this ILogger logger,
+        Exception exception,
+        [InterpolatedStringHandlerArgument("logger")] ref ZLoggerTraceInterpolatedStringHandler message)
+    {
+        logger.ZLogTrace(exception, ref message);
+    }
+
+
     public static void Debug(
         this ILogger logger,
         [InterpolatedStringHandlerArgument("logger")] ref ZLoggerDebugInterpolatedStringHandler message)
     {
         logger.ZLogDebug(ref message);
     }
+
+    public static void Debug(
+        this ILogger logger,
+        Exception exception,
+        [InterpolatedStringHandlerArgument("logger")] ref ZLoggerDebugInterpolatedStringHandler message)
+    {
+        logger.ZLogDebug(exception, ref message);
+    }
+
 
     public static void Info(
         this ILogger logger,
@@ -27,12 +45,30 @@ public static class LoggingExtensions
         logger.ZLogInformation(ref message);
     }
 
+    public static void Info(
+        this ILogger logger,
+        Exception exception,
+        [InterpolatedStringHandlerArgument("logger")] ref ZLoggerInformationInterpolatedStringHandler message)
+    {
+        logger.ZLogInformation(exception, ref message);
+    }
+
+
     public static void Warn(
         this ILogger logger,
         [InterpolatedStringHandlerArgument("logger")] ref ZLoggerWarningInterpolatedStringHandler message)
     {
         logger.ZLogWarning(ref message);
     }
+
+    public static void Warn(
+        this ILogger logger,
+        Exception exception,
+        [InterpolatedStringHandlerArgument("logger")] ref ZLoggerWarningInterpolatedStringHandler message)
+    {
+        logger.ZLogWarning(exception, ref message);
+    }
+
 
     public static void Error(
         this ILogger logger,
@@ -41,10 +77,27 @@ public static class LoggingExtensions
         logger.ZLogError(ref message);
     }
 
+    public static void Error(
+        this ILogger logger,
+        Exception exception,
+        [InterpolatedStringHandlerArgument("logger")] ref ZLoggerErrorInterpolatedStringHandler message)
+    {
+        logger.ZLogError(exception, ref message);
+    }
+
+
     public static void Crit(
         this ILogger logger,
         [InterpolatedStringHandlerArgument("logger")] ref ZLoggerCriticalInterpolatedStringHandler message)
     {
         logger.ZLogCritical(ref message);
+    }
+
+    public static void Crit(
+        this ILogger logger,
+        Exception exception,
+        [InterpolatedStringHandlerArgument("logger")] ref ZLoggerCriticalInterpolatedStringHandler message)
+    {
+        logger.ZLogCritical(exception, ref message);
     }
 }
