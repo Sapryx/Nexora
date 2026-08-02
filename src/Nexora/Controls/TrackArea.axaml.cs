0a6f454b174a8349fe05f0f861ff4d8e4058d1fa
sapryx <sapryxv@gmail.com>
2026-08-02 15:11:15 +0300

    configure track area

--- src/Nexora/Controls/TrackArea.axaml.cs
+++ src/Nexora/Controls/TrackArea.axaml.cs
@@ -1,14 +1,24 @@
+using System.Collections.ObjectModel;
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Nexora.ViewModels;
 
 namespace Nexora.Controls;
 
 public partial class TrackArea : UserControl
 {
+    public static readonly StyledProperty<ObservableCollection<AudioTrackVm>> TrackVmsProperty =
+        AvaloniaProperty.Register<TrackArea, ObservableCollection<AudioTrackVm>>(nameof(TrackVms)
+    );
+
+    public ObservableCollection<AudioTrackVm> TrackVms
+    {
+        get => GetValue(TrackVmsProperty);
+        set => SetValue(TrackVmsProperty, value);
+    }
+
     public TrackArea()
     {
         InitializeComponent();
     }
 }
-
